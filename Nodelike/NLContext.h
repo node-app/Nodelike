e42bab0467507f743522480e91832bd6e09d6b5c
Sam Rijs <sam@dev.local>
2013-12-31 00:37:09 +0100

    license: Mozilla Public License Version 2.0
    Chose this license over BSD or MIT because of copyleft clauses. Closes
    #3

--- Nodelike/NLContext.h
+++ Nodelike/NLContext.h
@@ -1,10 +1,13 @@
 //
 //  NLContext.h
-//  NodelikeDemo
+//  Nodelike
 //
 //  Created by Sam Rijs on 10/13/13.
-//  Copyright (c) 2013 Sam Rijs. All rights reserved.
+//  Copyright (c) 2013 Sam Rijs.
 //
+//  This Source Code Form is subject to the terms of the Mozilla Public
+//  License, v. 2.0. If a copy of the MPL was not distributed with this
+//  file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 #import <UIKit/UIKit.h>
 #import <JavaScriptCore/JavaScriptCore.h>
