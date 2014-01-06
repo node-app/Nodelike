aa93e75f13acdd8c9d7b9808c4b94121ed076d57
Sam Rijs <sam@dev.local>
2014-01-06 18:58:29 +0100

    process: clean up implementation

--- Nodelike/NLProcess.m
+++ Nodelike/NLProcess.m
@@ -11,44 +11,8 @@
 
 #import "NLProcess.h"
 
-#import "NLBinding.h"
-
 @implementation NLProcess {
     NSFileManager *filemngr;
-}
-
-- (id)init {
-
-    _platform = @"darwin";
-    _argv = NSProcessInfo.processInfo.arguments;
-    _env  = NSProcessInfo.processInfo.environment;
-
-    filemngr  = [NSFileManager new];
-
-    return [super init];
-
-}
-
-- (NSString *)cwd {
-    return filemngr.currentDirectoryPath;
-}
-
-- (void)chdir:(NSString *)path {
-    [filemngr changeCurrentDirectoryPath:path];
-}
-
-- (void)exit:(NSNumber *)code {
-    exit(code.intValue);
-}
-
-- (void)nextTick:(JSValue *)cb {
-    dispatch_async(dispatch_get_main_queue(), ^(void) {
-        [cb callWithArguments:@[]];
-    });
-}
-
-- (id)binding:(NSString *)binding {
-    return [NLBinding bindingForIdentifier:binding];
 }
 
 @end
