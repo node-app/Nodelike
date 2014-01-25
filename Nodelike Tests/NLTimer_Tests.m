1fa9019892ee3b15a121cdc98268ee538053119a
Sam Rijs <srijs@users.noreply.github.com>
2014-01-25 19:35:14 +0100

    add first test. still failing because of bundle issues.

---
+++ Nodelike Tests/NLTimer_Tests.m
@@ -0,0 +1,46 @@
+//
+//  NLTimer_Tests.m
+//  Nodelike
+//
+//  Created by Sam Rijs on 1/25/14.
+//  Copyright (c) 2014 Sam Rijs. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import "NLContext.h"
+
+@interface NLTimer_Tests : XCTestCase
+
+@end
+
+@implementation NLTimer_Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testSetTimeout {
+    __block bool canary = false;
+    NLContext *ctx = [NLContext new];
+    ctx[@"callback"] = ^{
+        canary = true;
+    };
+    ctx.exceptionHandler = ^(JSContext *ctx, JSValue *e) {
+        XCTFail(@"Context exception thrown: %@", e);
+    };
+    [ctx evaluateScript:@"require('timers').setTimeout(callback, 1000);"];
+    XCTAssertFalse(canary, @"Canary not false immediately after timeout set.");
+    [NSThread sleepForTimeInterval:1.0f];
+    XCTAssertFalse(canary, @"Canary not false immediately after timeout fired.");
+}
+
+@end
