ece66dd36422d740b5e56b2f606335d479f70bbb
Sam Rijs <srijs@users.noreply.github.com>
2014-01-29 23:10:59 +0100

    natives: read names from bundle

--- Nodelike/NLNatives.m
+++ Nodelike/NLNatives.m
@@ -13,67 +13,18 @@
 
 @implementation NLNatives
 
-+ (NSArray *)moduleNames {
-    return @[
-             @"_debugger",
-             @"_http_agent",
-             @"_http_client",
-             @"_http_common",
-             @"_http_incoming",
-             @"_http_outgoing",
-             @"_http_server",
-             @"_linklist",
-             @"_stream_duplex",
-             @"_stream_passthrough",
-             @"_stream_readable",
-             @"_stream_transform",
-             @"_stream_writable",
-             @"_tls_legacy",
-             @"_tls_wrap",
-             @"assert",
-             @"buffer",
-             @"child_process",
-             @"cluster",
-             @"console",
-             @"constants",
-             @"crypto",
-             @"dgram",
-             @"dns",
-             @"domain",
-             @"events",
-             @"freelist",
-             @"fs",
-             @"http",
-             @"https",
-             @"module",
-             @"net",
-             @"os",
-             @"path",
-             @"punycode",
-             @"querystring",
-             @"readline",
-             @"repl",
-             @"smalloc",
-             @"stream",
-             @"string_decoder",
-             @"sys",
-             @"timers",
-             @"tls",
-             @"tty",
-             @"url",
-             @"util",
-             @"vm",
-             @"zlib"
-             ];
-}
-
-+ (NSString *)source:(NSString *)module {
++ (NSBundle *)bundle {
     NSBundle *bundle     = [NSBundle bundleForClass:self.class];
     NSString *bundlePath = [bundle pathForResource:@"Nodelike" ofType:@"bundle"];
     if (bundlePath) {
-        bundle = [NSBundle bundleWithPath:bundlePath];
+        return [NSBundle bundleWithPath:bundlePath];
+    } else {
+        return bundle;
     }
-    NSString *path    = [bundle pathForResource:module ofType:@"js"];
+}
+
++ (NSString *)source:(NSString *)module {
+    NSString *path    = [self.bundle pathForResource:module ofType:@"js"];
     NSString *content = [NSString stringWithContentsOfFile:path
                                                   encoding:NSUTF8StringEncoding
                                                      error:nil];
@@ -81,11 +32,11 @@
 }
 
 + (id)binding {
-    NSArray  *names = self.moduleNames;
-    NSString *name;
-    JSValue  *sources = [JSValue valueWithNewObjectInContext:JSContext.currentContext];
-    for (int i = 0; i < names.count; i++) {
-        name = names[i];
+    NSArray *files   = [NSBundle pathsForResourcesOfType:@"js" inDirectory:self.bundle.bundlePath];
+    JSValue *sources = [JSValue valueWithNewObjectInContext:JSContext.currentContext];
+    for (int i = 0; i < files.count; i++) {
+        NSURL    *url  = files[i];
+        NSString *name = [url.lastPathComponent substringToIndex:url.lastPathComponent.length - url.pathExtension.length - 1];
         [sources defineProperty:name
                      descriptor:@{JSPropertyDescriptorGetKey: ^{ return [NLNatives source:name]; }}];
     }
