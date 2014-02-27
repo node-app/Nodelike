82a9510842039e2bbb2bfa64f54f890d6b44b958
Sam Rijs <srijs@airpost.net>
2014-02-27 02:30:03 +0100

    tests: nested module loading

--- Nodelike Tests/Resource_Tests.m
+++ Nodelike Tests/Resource_Tests.m
@@ -26,4 +26,27 @@
     NSLog(@"resourcePath=%@", pv.toString);
 }
 
+- (void)testResourcePathContainsTestModule {
+    NLContext *ctx = [NLContext new];
+    ctx.exceptionHandler = ^(JSContext *ctx, JSValue *e) {
+        XCTFail(@"Context exception thrown: %@; stack: %@", e, [e valueForProperty:@"stack"]);
+    };
+    JSValue *pv = [ctx evaluateScript:@"process.resourcePath"];
+    NSString *testMod = [NSString stringWithContentsOfFile:[pv.toString stringByAppendingString:@"/node_modules/test_module/index.js"] encoding:NSUTF8StringEncoding error:nil];
+    XCTAssertNotNil(testMod, @"test mod is nil.");
+}
+
+- (void)testRequireCanLoadTestModule {
+    NLContext *ctx = [NLContext new];
+    ctx.exceptionHandler = ^(JSContext *ctx, JSValue *e) {
+        XCTFail(@"Context exception thrown: %@; stack: %@", e, [e valueForProperty:@"stack"]);
+    };
+    JSValue *pv = [ctx evaluateScript:@"process.resourcePath"];
+    NSString *path = [pv.toString stringByAppendingString:@"/node_modules/test_module"];
+    NSString *script = [NSString stringWithFormat:@"require('module')._load('%@');", path];
+    JSValue *testMod = [ctx evaluateScript:script];
+    XCTAssertTrue([testMod valueForProperty:@"foo"].isString);
+    NSLog(@"testMod: %@", testMod.toDictionary);
+}
+
 @end
