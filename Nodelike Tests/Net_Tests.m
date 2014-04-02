b0c930325c95d3090c7c6f209555067e821f26d1
Sam Rijs <srijs@airpost.net>
2014-04-02 18:20:53 +0200

    tests: add some net tests

---
+++ Nodelike Tests/Net_Tests.m
@@ -0,0 +1,21 @@
+//
+//  Net_Tests.m
+//  Nodelike
+//
+//  Created by Sam Rijs on 4/2/14.
+//  Copyright (c) 2014 Sam Rijs. All rights reserved.
+//
+
+#import "NLTestCase.h"
+
+@interface Net_Tests : NLTestCase
+
+@end
+
+@implementation Net_Tests
+
+- (void)testAll {
+    [self runWithPrefix:@"test-net"];
+}
+
+@end
